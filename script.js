1cd433b64dcbe85857a1b0cbee11bc8b95252ea9
Pavel Sizykh <sizykh86@gmail.com>
2021-11-28 01:20:59 -0500

    commit

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const daySection = document.querySelectorAll(".schedule_section")
 
+let ratio = 0.35
+
 let allAppnt = {
 	monday: ["10:00 jhjkhgfggggfgfghfghfghfgh fghfghfghghfgfghfghfghfgfghfghfghfgfghfhgf", "11:00 fabfb", "12:00 rhekjrhu", "10:00 jhjkh", "11:00 fabfb", "12:00 rhekjrhu", "12:00 rhekjrhu"],
 	tuesday: ["09:00 ooo", "09:30 yy", "12:30 oopp"],
@@ -22,3 +24,27 @@ function addAppointments(){
 		}
 	}
 }
+
+function setSize() {
+  let scheduleSection = document.querySelectorAll(".schedule_section") //Result fields to set the right Height
+  let elementsHeight = new Array(...document.querySelectorAll(".name_day_of_week")) //Elements that need to be substracted from Window height
+	let sum = 0
+
+	for (let r of elementsHeight){
+		sum += r.clientHeight
+	}
+  // let sum = elementsHeight.reduce((acc, next) => acc.clientHeight + next.clientHeight) //Get sum of all elements to substract 
+  
+  //Set height of the result fields
+  for (let section of scheduleSection) {
+    section.style.height = `${(window.innerHeight - sum - 23) * ratio}px`
+		section.style.width = `${(window.innerWidth - 24) / 2}px`
+  }
+	document.querySelector("#top_line").style.width = `${window.innerWidth - 20 }px`
+}
+
+window.addEventListener('resize', () => setSize())
+
+
+setSize()
+addAppointments()
