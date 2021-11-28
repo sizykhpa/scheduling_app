fd6eca9a4c4f10eb051c840cec9cdfd2e8aa5759
Pavel Sizykh <sizykh86@gmail.com>
2021-11-28 00:35:14 -0500

    commit

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-const daySection = document.querySelectorAll(".day_of_week")
-
-let listOfTasks = ["jhjkh", "fabfb", "rhekjrhu"]
-let str = "sjhdsd"
+const daySection = document.querySelectorAll(".schedule_section")
 
 let allAppnt = {
-	monday: ["10:00 jhjkh", "11:00 fabfb", "12:00 rhekjrhu", "10:00 jhjkh", "11:00 fabfb", "12:00 rhekjrhu"],
+	monday: ["10:00 jhjkhgfggggfgfghfghfghfgh fghfghfghghfgfghfghfghfgfghfghfghfgfghfhgf", "11:00 fabfb", "12:00 rhekjrhu", "10:00 jhjkh", "11:00 fabfb", "12:00 rhekjrhu", "12:00 rhekjrhu"],
 	tuesday: ["09:00 ooo", "09:30 yy", "12:30 oopp"],
 	friday: ["09:00 ooertro", "09:30 yrtrty", "12:30 oowwpp"]
 }
